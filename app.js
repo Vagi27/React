a66c88dc62d69ba31b403238d26c831b036a5108
Vagish <vagishmlk@gmail.com>
2024-04-13 11:27:31 +0530

    mentioning proper usage of react Elements

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const heading1 = React.createElement("h1",
 
 const heading2 = React.createElement("h2",
     {
-        id: 'title', className: "heading2"
+        id: 'title', className: "heading2", style: { backgroundColor: "red" }
 
     }, "This is h2 tag");
 
 const container = React.createElement("div", {}, [heading1, heading2]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// react is rendered inside the created root element of reactDOM.
 
 root.render(container);
 
