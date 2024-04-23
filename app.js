b447be1089e82fbad30d9a8086ca4272f4e28944
Vagish <97430473+Vagi27@users.noreply.github.com.>
2024-04-23 13:46:36 +0530

    all different implementations of creating elements in react, using createElement, JSX, functional components, composition of components

--- app.js
+++ app.js
@@ -2,28 +2,104 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //react.createElement() => object => HTML code
-const heading1 = React.createElement(
-  "h1",
+
+// -----------------------------------------------------------------
+// create Element implementation
+const heading1 = React.createElement("h1", {}, "This is h1 tag");
+
+const heading2 = React.createElement("h2", {}, "this is h2 tag");
+const heading3 = React.createElement("h3", {}, "this is h3 tag");
+
+const container = React.createElement(
+  "div",
   {
-    id: "title",
-    className: "heading1",
+    className: "title",
   },
-  "This is h1 tag"
+  [heading1, heading2, heading3]
 );
 
-console.log(heading1);
+// console.log(heading1);
+// -----------------------------------------------------------------
 
-const heading2 = (
-  <h1>
-    Namastey React
-    <h2>this is h2</h2>
-  </h1>
-); //JSX
+// -----------------------------------------------------------------
+//JSX element implementation:
+const heading4 = (
+  <div className="title">
+    <h1>this is h1 tag</h1>
+    <h2>this is h2 tag</h2>
+    <h3>this is h3 tag</h3>
+  </div>
+);
+// -----------------------------------------------------------------
 
-const container = React.createElement("div", {}, [heading1, heading2]);
+/* difference b/w jsx element and functional component is just that:
+ ->  component is declared as a function, whereas, an element is not. */
 
-// react is rendered inside the created root element of reactDOM.
+/* In JSX, lower-case tag names are considered to be HTML tags. However, lower-case tag names with a dot (property accessor) aren't.
 
+*-> See HTML tags vs React Components.
+
+* <component /> compiles to React.createElement('component') (html tag)
+* <Component /> compiles to React.createElement(Component)
+* <obj.component /> compiles to React.createElement(obj.component)
+*/
+
+// -----------------------------------------------------------------
+//functional Component implementation:
+/*
+ *-> heading5() with lower case letters can be rendered.
+ *-> <heading5 /> in small case. cannot be rendered.
+ *-> since lower case name of components will be treated as html tags.
+ *-> First letter of component must be capital to render it using angular brackets. */
+const Heading5 = () => {
+  return (
+    <div className="title">
+      <h1>this is h1 tag</h1>
+      <h2>this is h2 tag</h2>
+      <h3>this is h3 tag</h3>
+    </div>
+  );
+};
+// -----------------------------------------------------------------
+
+// -----------------------------------------------------------------
+// equivalent of functional component implementation
+
+const Heading6 = () => (
+  <div className="title">
+    <h1>this is h1 tag</h1>
+    <h2>this is h2 tag</h2>
+    <h3>this is h3 tag</h3>
+  </div>
+);
+
+// -----------------------------------------------------------------
+
+// -----------------------------------------------------------------
+//implementation with angular brackets did not work until first letter of functional Component is made capital
+// Composition of Component (Add a component inside another)
+// or 
+//Nested components or component composition
+const Heading7 = () => (
+  <div>
+    {<Heading6 />}
+    <h4> this is h4 tag</h4>
+  </div>
+);
+// -----------------------------------------------------------------
+
+// react is rendered inside the created root element of ReactDOM.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+// root.render(container);
+
+// root.render(heading4);
+
+// root.render(Heading5()); or root.render(<Heading5/>);
+
+// root.render(<Heading5 />);
+
+// root.render(Heading6());
+// root.render(<Heading6 />);
+
+root.render(<Heading7 />);
