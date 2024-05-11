b06bb4101e9a66e1dd84f0b8022c7893dadb464d
Vagish <97430473+Vagi27@users.noreply.github.com.>
2024-05-12 00:58:15 +0530

    making a 3 component app with Header+Body+Footer Components

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Header = () => {
+const Title = () => {
   return (
-    <div className="Header flex">
+    <a href="/">
       <img
         src={require("./images/company-logo.jpg")}
         alt="Company logo"
         className="logo"
       />
+    </a>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="Header flex">
+      <Title />
 
       <input type="text" className="headerInput" placeholder="Search.." />
 
@@ -21,7 +29,46 @@ const Header = () => {
   );
 };
 
+const restaurantList=[{}];
+const RestaurantCard = () => (
+  <div className="card">
+    <img src="https://img.freepik.com/free-photo/grilled-gourmet-burger-with-cheese-tomato-onion-french-fries-generated-by-artificial-intelligence_25030-63181.jpg" />
+    <h2>burger King</h2>
+    <h3>tags</h3>
+    <h3>stars</h3>
+  </div>
+);
+
+const Body = () => (
+  <div className="restaurantContainer flex ">
+
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+  </div>
+);
+const Footer = () => <></>;
+
+const AppLayout = () => (
+  <> {/* React Fragment */}
+    <Header />
+    <Body />
+    <Footer />
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(<Header />);
-root.render(<Jsx2 />);
+root.render(<AppLayout />);
