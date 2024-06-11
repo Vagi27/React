51309358d96f239a7ba11de377b9f22bf109971f
Vagish <97430473+Vagi27@users.noreply.github.com.>
2024-06-12 03:10:38 +0530

    little changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,34 +10,18 @@ import { CDN_ALL_RESTAURANTS } from "../constants";
 const Body = () => {
   const [searchText, setSearchText] = useState("");
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-  const [allRestaurants, setAllRestaurants] = useState([]);
 
-  // useEffect(() => {
+  const allRestaurants = useAllRestaurants();
 
-  //   const {allRestaurants} = useAllRestaurants();
-  //   setFilteredRestaurants(allRestaurants);
-  // }, []);
   useEffect(() => {
-    async function fetchData() {
-      console.log("fetch API");
-      try {
-        const data = await fetch(CDN_ALL_RESTAURANTS);
-        const info = await data.json();
-        const restaurants =
-          info?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
-            ?.restaurants || [];
-        setAllRestaurants(restaurants);
-        setFilteredRestaurants(restaurants);
-      } catch (err) {
-        console.error("Error fetching data: " + err);
-      }
+    if (allRestaurants.length > 0) {
+      // this condition is saving 1 full render. because in cases where allrestaurants isn't fetched it is still updating local state variable, which causes render
+      setFilteredRestaurants(allRestaurants);
     }
-    fetchData();
-  }, []);
+  }, [allRestaurants]);
 
   // console.log("iteration");
   // console.log(allRestaurants);
-
 
   return (
     <>
@@ -59,7 +43,6 @@ const Body = () => {
         >
           Search
         </button>
-
       </div>
       {filteredRestaurants.length === 0 ? (
         <Shimmer />
@@ -73,7 +56,6 @@ const Body = () => {
               <RestaurantCard {...restaurant?.info} />
             </Link>
           ))}
-
         </div>
       )}
     </>
--- src/components/Utility/useAllRestaurants.js
+++ src/components/Utility/useAllRestaurants.js
@@ -3,8 +3,10 @@ import { CDN_ALL_RESTAURANTS } from "../../constants";
 
 function useAllRestaurants() {
   const [allRestaurants, setAllRestaurants] = useState([]);
-
+  // console.log("other file");
+  
   useEffect(() => {
+    // console.log("other file's useEffect");
     fetchData();
   }, []);
 
@@ -15,6 +17,7 @@ function useAllRestaurants() {
       const restaurants =
         info?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
           ?.restaurants || [];
+          // console.log(restaurants);
       setAllRestaurants(restaurants);
     } catch (err) {
       console.error("Error fetching data: " + err);
